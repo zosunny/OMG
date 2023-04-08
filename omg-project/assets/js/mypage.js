db2830419ff2d178b47ebf9f70d8b399641610ba
chlgksdbs <chlgksdbs98@naver.com>
2023-04-08 14:57:14 +0900

    feat: 마이 페이지 버튼 UI 구현

--- omg-project/assets/js/mypage.js
+++ omg-project/assets/js/mypage.js
@@ -1,6 +1,3 @@
-var submit = document.getElementById("submit_button");
-submit.onclick = showImage; // submit 버튼 클릭 시, 이미지 보여주기
-
 function loadFile(input) {
     var file = input.files[0]; // 선택된 파일 불러오기
 
@@ -13,21 +10,15 @@ function loadFile(input) {
 
     newImage.style.width = "200px";
     newImage.style.height = "200px";
-    newImage.style.visibility = "hidden"; // 버튼 클릭 전, 이미지 숨기기
     newImage.style.objectFit = "contain";
 
     // 이미지를 myImg div에 추가
     var container = document.getElementById("myImg");
-    container.appendChild(newImage);
-}
 
-function showImage() {
-    var newImage = document.getElementById("myImg").lastElementChild;
-
-    // 이미지 화면 출력
-    newImage.style.visibility = "visible";
-
-    // 이미지 업로드 버튼 숨기기
-    document.getElementById("image_upload").style.visibility = "hidden";
-    document.getElementById("myText").textContent = null; // 기존 파일 이름 지우기
+    if (container.firstChild) { // 이미지가 존재하는 경우, 삭제 후 등록
+        container.textContent = "";
+        container.appendChild(newImage);
+    } else { // 이미지가 존재하지 않는 경우, 등록
+        container.appendChild(newImage);
+    }
 }
